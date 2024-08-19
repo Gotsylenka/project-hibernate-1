3eaacfacc001ce6a4c8fd0a10ebaf1d91b9e06d4
Darya <gotsdarya@gmail.com>
2024-08-19 21:58:40 +0200

    project done

--- src/main/java/com/game/config/MyWebAppInit.java
+++ src/main/java/com/game/config/MyWebAppInit.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+
 public class MyWebAppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,51 +1,103 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+
+import jakarta.annotation.PreDestroy;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.PreDestroy;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
-    public PlayerRepositoryDB() {
+    private final SessionFactory sessionFactory;
 
+    public PlayerRepositoryDB() {
+        Properties props = new Properties();
+        props.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        props.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        props.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg");
+        props.put(Environment.USER, "root");
+        props.put(Environment.PASS, "admin");
+        props.put(Environment.HBM2DDL_AUTO, "update");
+
+        sessionFactory = new Configuration()
+                .addAnnotatedClass(Player.class)
+                .setProperties(props)
+                .buildSessionFactory();
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+        String sql = "SELECT * FROM rpg.player";
+
+        try(Session session = sessionFactory.openSession()) {
+            NativeQuery<Player> query = session.createNativeQuery(sql, Player.class);
+            query.setFirstResult(pageNumber * pageSize);
+            query.setMaxResults(pageSize);
+            return query.list();
+        }
     }
 
     @Override
     public int getAllCount() {
-        return 0;
+
+        try(Session session = sessionFactory.openSession()) {
+            Query<Long> namedQuery = session.createNamedQuery("playerCountAll", Long.class);
+            return namedQuery.uniqueResult().intValue();
+        }
     }
 
     @Override
     public Player save(Player player) {
-        return null;
+        try(Session session = sessionFactory.openSession()) {
+            Transaction trans = session.beginTransaction();
+            session.save(player);
+            trans.commit();
+            return player;
+        }
     }
 
     @Override
     public Player update(Player player) {
-        return null;
+        try(Session session = sessionFactory.openSession()) {
+            Transaction trans = session.beginTransaction();
+            session.update(player);
+            trans.commit();
+            return player;
+        }
     }
 
     @Override
     public Optional<Player> findById(long id) {
-        return Optional.empty();
+        try(Session session = sessionFactory.openSession()) {
+            Query<Player> query = session.createQuery("SELECT p FROM Player p WHERE p.id =:id ", Player.class);
+            return Optional.ofNullable(session.get(Player.class, id));
+        }
     }
 
     @Override
     public void delete(Player player) {
-
+        try(Session session = sessionFactory.openSession()) {
+            Transaction trans = session.beginTransaction();
+            session.delete(player);
+            trans.commit();
+        }
     }
 
     @PreDestroy
     public void beforeStop() {
-
+        sessionFactory.close();
     }
 }
